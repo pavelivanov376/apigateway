1fab835639d3a4e45f4af0d2f341877753a9f55d
Pavel Ivanov <pavelivanov376@gmail.com>
2025-09-27 14:55:42 +0300

    fix component name

--- ui/src/components/FileListingContainer.js
+++ ui/src/components/FileListingContainer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import FileTable from "./FileTable";
 
-export default function FileListContainer() {
+export default function FileListingContainer() {
   const [content, setContent] = useState([]);
   //TODO: Replace with actual directory ID from routing or props
   //TODO: See how to get current directory ID for curent user root directory
@@ -16,13 +16,6 @@ export default function FileListContainer() {
       .then((data) => setContent(data))
       .catch((error) => {
         console.error("Error fetching files:", error);
-        //TODO: Remove Fallback data for demonstration purposes
-        setContent([
-          { id: 1, name: "contract.pdf", size: "1.5 MB", creationDate: "2025-06-25" },
-          { id: 2, name: "invoice.xlsx", size: "430 KB", creationDate: "2025-06-24" },
-          { id: 3, name: "present.pptx", size: "3.1 MB", creationDate: "2025-06-23" },
-          { id: 3, name: "present.pptx", size: "3.1 MB", creationDate: "2025-06-23" },
-        ]);
       });
   }, []);
 
