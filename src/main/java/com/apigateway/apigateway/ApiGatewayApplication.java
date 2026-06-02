9c5e095173bcc5adbbe5bb327b745aca9f860f9d
Pavel Ivanov <pavelivanov376@gmail.com>
2026-06-02 16:45:27 +0300

    add docker-compose setup

--- src/main/java/com/apigateway/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/apigateway/apigateway/ApiGatewayApplication.java
@@ -1,6 +1,7 @@
 package com.apigateway.apigateway;
 
 import com.apigateway.apigateway.filters.ServiceTokenGatewayFilterFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -9,6 +10,12 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
+
+    @Value("${app.fileservice.url}")
+    private String fileserviceUrl;
+
+    @Value("${app.folderservice.url}")
+    private String folderserviceUrl;
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
@@ -23,21 +30,21 @@ public class ApiGatewayApplication {
                         .path("/api/download/{id}")
                         .filters(f -> f.rewritePath("/api/download/(?<id>.*)", "/api/download/${id}")
                                 .filter(serviceTokenFilter.apply(new ServiceTokenGatewayFilterFactory.Config())))
-                        .uri("http://fileservice:80")
+                        .uri(fileserviceUrl)
                 )
 
                 // Route /api/upload to File Service
                 .route("upload-file-request", r -> r
                         .path("/api/upload")
                         .filters(f -> f.filter(serviceTokenFilter.apply(new ServiceTokenGatewayFilterFactory.Config())))
-                        .uri("http://fileservice:80")
+                        .uri(fileserviceUrl)
                 )
 
                 // Route /api/file/{uuid} DELETE to File Service
                 .route("delete-file-request", r -> r
                         .path("/api/file/{uuid}")
                         .filters(f -> f.filter(serviceTokenFilter.apply(new ServiceTokenGatewayFilterFactory.Config())))
-                        .uri("http://fileservice:80")
+                        .uri(fileserviceUrl)
                 )
 
                 //ROUTES FOR FOLDER SERVICE
@@ -47,7 +54,7 @@ public class ApiGatewayApplication {
                         .path("/api/folder/create")
                         .filters(f -> f
                                 .filter(serviceTokenFilter.apply(new ServiceTokenGatewayFilterFactory.Config())))
-                        .uri("http://folderservice:80")
+                        .uri(folderserviceUrl)
                 )
 
                 // Route /folder/{id} to Folder Service
@@ -55,7 +62,7 @@ public class ApiGatewayApplication {
                         .path("/api/folder/{id}")
                         .filters(f -> f.rewritePath("/api/folder/(?<id>.*)", "/api/folder/${id}")
                                 .filter(serviceTokenFilter.apply(new ServiceTokenGatewayFilterFactory.Config())))
-                        .uri("http://folderservice:80")
+                        .uri(folderserviceUrl)
                 )
 
                 // Route /folder/create/{id} to Folder Service
@@ -63,14 +70,14 @@ public class ApiGatewayApplication {
                         .path("/api/folder/create/{id}")
                         .filters(f -> f.rewritePath("/api/folder/(?<id>.*)", "/api/folder/${id}")
                                 .filter(serviceTokenFilter.apply(new ServiceTokenGatewayFilterFactory.Config())))
-                        .uri("http://folderservice:80")
+                        .uri(folderserviceUrl)
                 )
 
                 // Route /api/file/link/folder to Folder Service
                 .route("link-file-to-parent-folder", r -> r
                         .path("/api/file/link/folder")
                         .filters(f -> f.filter(serviceTokenFilter.apply(new ServiceTokenGatewayFilterFactory.Config())))
-                        .uri("http://folderservice:80")
+                        .uri(folderserviceUrl)
                 )
 
                 .build();
